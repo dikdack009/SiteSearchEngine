84b1f0b409d7a7e2b40a34df4f77ae867b5ab39b
Вадим <vadim.murov@mail.ru>
2023-04-12 00:50:48 +0300

    Добавлен метод получения логина по токенам

--- src/main/java/pet/diploma/sitesearchengine/controller/AuthController.java
+++ src/main/java/pet/diploma/sitesearchengine/controller/AuthController.java
@@ -39,7 +39,9 @@ public class AuthController {
         return token.getError() == null ? ResponseEntity.ok(token) : new ResponseEntity<>(token, HttpStatus.FORBIDDEN);
     }
 
+    @PostMapping("getAuthInfo")
     public ResponseEntity<String> getLoginByToken(@RequestBody @NotNull JwtRequest authRequest) {
         return ResponseEntity.ok(authRequest.getLogin());
+//        TODO: Проверить при неправильных токенах
     }
 }
