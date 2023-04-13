e92a07240752109ec1ab828a132125820e2471c9
Вадим <vadim.murov@mail.ru>
2023-04-13 22:09:03 +0300

    Исправлен относительный путь к картинкам для письма уведомления

--- src/main/java/pet/diploma/sitesearchengine/security/JwtProvider.java
+++ src/main/java/pet/diploma/sitesearchengine/security/JwtProvider.java
@@ -71,13 +71,13 @@ public class JwtProvider {
                     .parseClaimsJws(token);
             return true;
         } catch (ExpiredJwtException expEx) {
-            log.error("Token expired", expEx);
+            log.error("Token expired");
         } catch (UnsupportedJwtException unsEx) {
-            log.error("Unsupported jwt", unsEx);
+            log.error("Unsupported jwt");
         } catch (SignatureException sEx) {
-            log.error("Invalid signature", sEx);
+            log.error("Invalid signature");
         } catch (Exception e) {
-            log.error("invalid token", e);
+            log.error("invalid token");
         }
         return false;
     }
