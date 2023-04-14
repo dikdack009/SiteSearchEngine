70da6e2d6bb90bf7f27b7b425e5703c154d0f675
Вадим <vadim.murov@mail.ru>
2023-04-14 22:30:39 +0300

    no args constructor

--- src/main/java/pet/diploma/sitesearchengine/security/JwtRequest.java
+++ src/main/java/pet/diploma/sitesearchengine/security/JwtRequest.java
@@ -2,11 +2,13 @@ package pet.diploma.sitesearchengine.security;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class JwtRequest {
 
     private String login;
